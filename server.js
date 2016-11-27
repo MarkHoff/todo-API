1cc49bb0bf13611c25c3d9ea8af6bab5d171ad3b
MarkHoff <mark.hoffman9@gmail.com>
2016-11-27 10:57:27 -0500

    Added bodyParser

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-	
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Pick up car',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 //GET /todos
 app.get('/todos', function(req, res) {
@@ -40,6 +31,18 @@ app.get('/todos/:id', function(req, res){
 	} else {
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	
+	todos.push(body);
+	console.log(todos);
+	
+	console.log('description: ' + body.description);
+	res.json(body);
+	
 });
 
 app.listen(PORT, function() {
