8ead56de0c4c08ac865b9f36b72654276dcf2787
MarkHoff <mark.hoffman9@gmail.com>
2016-12-02 18:08:02 -0500

    Final todos version

--- server.js
+++ server.js
@@ -12,12 +12,20 @@ app.use(bodyParser.json());
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
+	var index;
+	//var body = _.pick(req.body, ['description']);
 	
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
-	}										
+	}	
+	
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1
+		});
+	}
 	res.json(filteredTodos);
 });
 
