fd7abaa6d80be61705d2c691319d2c5dd383ba34
MarkHoff <mark.hoffman9@gmail.com>
2016-12-08 22:10:18 -0500

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
+var users = [];
 var todoNextId = 1;
 var _ = require('underscore');
 var db = require('./db.js');
@@ -106,6 +107,15 @@ app.put('/todos/:id', function(req, res) {
 	}, function() {
 			res.status(500).send();
 	});
+});
+
+app.post('/users', function(req, res) {
+		var body = _.pick(req.body, ['email', 'password']); 
+		db.user.create(body).then(function(user) {
+				res.json(user.toJSON());
+		}, function(e) {
+				res.status(400).json(e);
+		});
 });
 
 db.sequelize.sync().then(function(){
